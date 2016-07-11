9cef10508e207f98fa8c078575a7f9cc371457a8
suprsidr <suprsidr@gmail.com>
2016-07-11 11:45:26 -0500

    add modified and modifiedby

--- students-in.js
+++ students-in.js
@@ -8,7 +8,7 @@ var ItemProvider = require('./itemprovider-mongodb').ItemProvider,
     uuid = require('uuid'),
     data = require('./students').results;
 
-var i = 0, l = data.length;
+var i = 0, l = data.length, date = Date.now();
 
 var majors = ["Agriculture","Agriculture Operations","Architecture","Area","Ethnic","Cultural","Gender","Aviation","Biological and Biomedical Sciences","Business","Management","Marketing","Communication","Journalism","Communications Technologies","Computer and Information Sciences","Construction Trades","Education","Engineering Technologies","Engineering","English Language","Family and Consumer Sciences","Foreign Languages","Literatures","Health Professions","History","Homeland Security","Law Enforcement","Firefighting","Human Services","Legal Professions","Liberal Arts","Humanities","Library Science","Mathematics and Statistics","Mechanic and Repair Technologies","Military Technologies and Applied Sciences","Multi/interdisciplinary Studies","Natural Resources and Conservation","Parks"," Recreation"," Leisure and Fitness Studies","Personal and Culinary Services","Philosophy and Religious Studies","Physical Sciences","Precision Production","Psychology","Science Technologies","Social Sciences","Theology and Religious Vocations","Transportation and Materials Moving","Visual and Performing Arts"];
 function getRandomFloatInclusive(min, max) {
@@ -22,6 +22,8 @@ function saveToDb() {
   data[i].sid = uuid.v4();
   data[i].gpa = getRandomFloatInclusive(2.5, 4.0).toFixed(1);
   data[i].major = majors[getRandomIntInclusive(0, majors.length - 1)];
+  data[i].modified = date;
+  data[i].modifiedby = 'Wayne Patterson';
   itemProvider.findOne({collection: 'students', query: {sid: data[i].sid}}, function(err, item) {
     if(err){
       console.log(err);
@@ -57,5 +59,12 @@ function saveToDb() {
 
 // open DB -> insert items into DB
 itemProvider.open(function(){
-  saveToDb();
+  itemProvider.getCollection('students', function(err, collection) {
+    if(err) {
+      console.log(err);
+      return;
+    }
+    collection.createIndex( { sid: 1 } )
+    saveToDb();
+  });
 });
